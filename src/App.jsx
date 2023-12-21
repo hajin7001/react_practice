825d633e61373f0903cb2ced4045c9467e45da55
Hajin Yun <faiith7001@gmail.com>
2023-12-21 14:08:48 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ import { useState } from "react"
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 import GameOver from "./components/GameOver.jsx";
 
+const PLAYERS = {
+  X:'Player 1',
+  O: 'Player 2'
+};
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
 
@@ -14,24 +25,8 @@ function deriveActivePlayer(gameTurns){
   return currentPlayer;
 }
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
-
-function App() {
-  const [players, setPlayers] = useState({
-    'X' : 'Player 1',
-    'O' : 'Player 2'
-  });
-  
-  const [gameTurns, setGameTurns] = useState([]);
-  //const [activePlayer, setActivePlayer] = useState('X');
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
+function deriveGameBoard(gameTurns){
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
 
   for(const turn of gameTurns){
     const { square, player } = turn;
@@ -39,7 +34,10 @@ function App() {
 
     gameBoard[row][col] = player;
   }
+  return gameBoard;
+}
 
+function deriveWinner(gameBoard, players){
   let winner = null;
 
   for (const combination of WINNING_COMBINATIONS){
@@ -47,20 +45,27 @@ function App() {
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
-    console.log(firstSquareSymbol, secondSquareSymbol, thirdSquareSymbol);
     // 이 3가지가 모두 동일한 player인지 확인
     if(firstSquareSymbol 
       && firstSquareSymbol === secondSquareSymbol 
       && firstSquareSymbol === thirdSquareSymbol){
-        winner = firstSquareSymbol;
-        console.log(winner);
+        winner = players[firstSquareSymbol]
       }
   }
+  return winner;
+}
 
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length == 9 && !winner;
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
       const currentPlayer = deriveActivePlayer(prevTurns);
 
@@ -74,14 +79,34 @@ function App() {
   function handleRestart(){
     setGameTurns([]);
   }
+
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]:newName
+        // 바뀐 경우에 대해서만 overwrite해준다고 생각
+      };
+    })
+  }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}></Player>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}></Player>
+          <Player 
+            initialName={PLAYERS.X} 
+            symbol="X" 
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player 
+            initialName={PLAYERS.O}
+            symbol="O" 
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
-        {(winner || hasDraw)&& <GameOver winner={winner} onRestart={handleRestart}/>}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}activePlayerSymbol={activePlayer}/>
       </div>
       <Log turns={gameTurns}/>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 
-export default function Player({initialName, symbol, isActive}){
+export default function Player({initialName, symbol, isActive, onChangeName}){
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   const handleEditClick = () => {
     setIsEditing((editing) => !editing);
+
+    if(isEditing)
+      onChangeName(symbol, playerName);
   }
 
   const handleChange = (event) => {
     setPlayerName(event.target.value);
   };
+  
   const editablePlayerName = <span className="player-name">{playerName}</span>;
   
   return (
